b9e73ee4fd30d2564db6c4b03cabc4eafd40682e
Bendt Schulz <bendt@schulz-hamburg.de>
2023-05-28 14:08:01 +0200

    addedd deletion protection to state and lock setup

--- main.tf
+++ main.tf
@@ -33,6 +33,10 @@ provider "aws" {
 # bucket configuration
 resource "aws_s3_bucket" "tf-state-bucket-backend" {
   bucket = var.state_bucket_name
+
+  lifecycle {
+    prevent_destroy = true
+  }
 }
 
 # enable versioning for the bucket
@@ -41,6 +45,10 @@ resource "aws_s3_bucket_versioning" "tf-state-bucket-versioning" {
 
   versioning_configuration {
     status = "Enabled"
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 }
 
@@ -52,6 +60,10 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "tf-state-bucket-e
     apply_server_side_encryption_by_default {
       sse_algorithm = "aws:kms"
     }
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 }
 
@@ -65,5 +77,9 @@ resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
   attribute {
     name = "LockID"
     type = "S"
+  }
+
+  lifecycle {
+    prevent_destroy = true
   }
 }
